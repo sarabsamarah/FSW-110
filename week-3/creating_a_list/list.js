0ade42e88baa3ee40b648dd579722515e1c9ca22
sarabsamarah <sarab.samarah@bryanuniversity.edu>
2021-03-04 00:34:09 -0800

    file upload

---
+++ week-3/creating_a_list/list.js
@@ -0,0 +1,33 @@
+var newH1 = document.createElement("h1")
+newH1.textContent = "Hello World"
+document.body.appendChild(newH1)
+
+for (newH1 = 0; newH1 < 9; newH1 ++) {
+ var repH1 = document.createElement("h1");
+ repH1.innerHTML = "Hello World";
+ repH1.style.color = "magenta";
+ repH1.style.textAlign = "center"
+ repH1.style.backgroundColor = "turquoise"
+ document.body.appendChild(repH1)
+}
+
+const names = [
+    "steve",
+    "larry",
+    "joe",
+    "shirley",
+    "steph",
+    "nate",
+    "rick",
+    "emily"
+]
+var myList = document.getElementById("names")
+myList.textContent = "List Of Names"
+
+for (var i = 0; i < names.length; i++) {
+    var newAdd = document.createElement("li")
+    myList.innerHTML += '<li>' + names[i] + '</li>'
+    myList.style.backgroundColor = "yellow";
+    myList.style.color = "navy";
+    document.body.appendChild(myList)
+}
