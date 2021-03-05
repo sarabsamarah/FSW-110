3ac800df5f840da04d2dc83a0ebda74e17de87c2
sarabsamarah <sarab.samarah@bryanuniversity.edu>
2021-03-05 13:23:22 -0800

    test

--- week-3/creating_styles_wJS/create_styles.js
+++ week-3/creating_styles_wJS/create_styles.js
@@ -1,19 +1,12 @@
-
-// var newH2 = document.createElement("h2")
-// newH2.textContent = "Introduction to Yoga Asana"
-
 for (listH2 = 0; listH2 < 5; listH2++) {
-    const reListH2 = document.createElement("h2");
-    reListH2.innerHTML = "Yoga Asana";
+    const repListH2 = document.createElement("h2");
+    repListH2.innerHTML = "Yoga Asana";
 
-reListH2.style.fontSize = "20px";
-reListH2.style.fontWeight = "lighter";
-reListH2.style.fontFamily = "sans-serif";
-reListH2.style.color= "cornflowerblue";
-reListH2.classList.add("border");
-document.body.appendChild(reListH2);
+repListH2.style.fontSize = '20px';
+repListH2.style.fontWeight = 'lighter';
+repListH2.style.fontFamily = 'ans-serif';
+repListH2.style.color= 'cornflowerblue';
+repListH2.classList.add("border");
+document.body.appendChild(repListH2);
 
 }
-
-
-
