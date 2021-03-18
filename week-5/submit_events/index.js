95e5a3fbbba2d59fdeecf8256496ec88c8f55ece
sarabsamarah <sarab.samarah@bryanuniversity.edu>
2021-03-17 23:02:36 -0700

    submit events

---
+++ week-5/submit_events/index.js
@@ -0,0 +1,10 @@
+const inputElement = document.getElementById('text-input')
+
+function myFunction() {
+    alert("Please enter your information in boxes!  Good Luck!")
+  }
+  function submit()
+  {
+      alert("click ok to finalize entry");
+      location.reload();
+  }
