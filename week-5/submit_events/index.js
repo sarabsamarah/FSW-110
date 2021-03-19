717bb05955d468eedc20b9da81d2067a17257a80
sarabsamarah <sarab.samarah@bryanuniversity.edu>
2021-03-19 00:04:56 -0700

    modify elements

--- week-5/submit_events/index.js
+++ week-5/submit_events/index.js
@@ -1,10 +1,31 @@
-const inputElement = document.getElementById('text-input')
 
 function myFunction() {
-    alert("Please enter your information in boxes!  Good Luck!")
+    alert("Please follow instruction below! Good Luck!")
   }
+
+  // // function confirming() {
+  //   var a = document.getElementById("nameText").value;
+  //   // alert("Please confirm: " + a);
+    
+  // // }
+  // // function confirming() {
+    
+  //   // alert("Please confirm: " + b);
+    
+  // // }
+  function confirming() {
+    var a = document.getElementById("nameText").value;
+    var b = document.getElementById("lastText").value;
+    var c = document.getElementById("phoneNum").value;
+    alert("Please confirm: "  + a  +  b  +   c);
+    }
+
+  
+  
   function submit()
   {
       alert("click ok to finalize entry");
       location.reload();
   }
+
+
