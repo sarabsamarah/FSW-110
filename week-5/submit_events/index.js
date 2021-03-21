03c4bca31ad82b7db0788f72f8365ecfdf45ea46
sarabsamarah <sarab.samarah@bryanuniversity.edu>
2021-03-21 00:32:47 -0700

    Revised Files

--- week-5/submit_events/index.js
+++ week-5/submit_events/index.js
@@ -13,13 +13,23 @@
   //   // alert("Please confirm: " + b);
     
   // // }
-  function submit() {
-    var a = document.getElementById("nameText").value;
-    var b = document.getElementById("lastText").value;
-    var c = document.getElementById("phoneNum").value;
-    result = (a+ " " + b+  " "+ c+ " ");
-      alert("Please confirm:  "+ result);
-      location.reload();
-  }
+  // function submit() {
+  //   var a = document.getElementById("nameText").value;
+  //   var b = document.getElementById("lastText").value;
+  //   var c = document.getElementById("phoneNum").value;
+  //   result = (a+ " " + b+  " "+ c+ " ");
+  //     alert("Please confirm:  "+ result);
+  //     location.reload();
+  // }
+
+  const form = document.form;
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    alert
+    (`
+    Name: ${form.nameText.value} ${form.lastText.value}
+    Phone Number: ${form.phoneNum.value}`);
+    location.reload();
+    })
 
 
